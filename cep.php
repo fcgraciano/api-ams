5c02e15f53875a7f8870b873c444f29b3f7e7d84
Prof Fernando <fcgraciano@gmail.com>
2025-08-13 17:30:10 -0300

    cep

--- cep.php
+++ cep.php
@@ -6,11 +6,28 @@
     <title>Consultar Cep</title>
 </head>
 <body>
+    CEP:
     <input name="cep" id="cep" type="text" />
-    <button onclick="">Consultar Cep</button>
+    <button onclick="ConsultarCEP()">Consultar Cep</button>
+    <br/>
+    Endereço: <input type="text" id="endereco" />
+    <br/>
+    Cidade: <input type="text" id="cidade" />
+    <br/>
+    Estado: <input type="text" id="estado" />
+    <br/>
     <script>
-        var cep = document.getElementById("cep")
-
+        
+        function ConsultarCEP()
+        {
+            var cep = document.getElementById("cep")
+            let url = "https://viacep.com.br/ws/"+cep.value+"/json/"
+            fetch(url)
+            .then( resp => resp.json())
+            .then(dados => {
+                alert(dados.logradouro)
+            })
+        }
     </script>
 </body>
 </html>
